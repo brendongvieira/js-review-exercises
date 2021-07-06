f36a5da77edfaaddae3c7d76229ea98007654909
Brendon Vieira <brendon-vieira@live.com>
2021-07-06 20:49:54 -0300

    First commit

---
+++ example14.js
@@ -0,0 +1,7 @@
+// Print on the screen the even numbers between 0 and 100.
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
---
+++ example34.js
@@ -0,0 +1,23 @@
+// Create an algorithm that receives three scores from a student, calculate its average and show the following messages accordingly:
+
+// - If the average is equal or greater than 7 - Approved
+// - If the average is greater and equal to 5 and less than 7 - Retake Test
+// - If average is less than 5 - Failed
+
+function calculateAverage(score1, score2, score3) {
+  let averageGrade = (score1 + score2 + score3) / 3;
+
+  if (averageGrade >= 7) {
+    return "Approved";
+  }
+
+  if (averageGrade >= 5 && averageGrade < 7) {
+    return "Retake Test";
+  }
+
+  if (averageGrade < 5) {
+    return "Failed";
+  }
+}
+
+console.log(calculateAverage(8, 5, 8));
